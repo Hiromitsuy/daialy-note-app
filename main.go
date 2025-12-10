8cef63a9afb6656474ec3f566318aafb6b8352d8
lunasky-hy <19776220+lunasky-hy@users.noreply.github.com>
2025-12-10 13:58:34 +0900

    delete mock data

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"github.com/lunasky-hy/dialy-note-app/src/controller"
 	"github.com/lunasky-hy/dialy-note-app/src/database"
@@ -12,30 +11,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
-
-type question struct {
-	Id int `json:"id"`
-	QText string `json:"qtext"`
-}
-
-type daiary struct {
-	Id int `json:"id"`
-	Note string `json:"note"`
-	UserId int `json:"userId"`
-	Question question `json:"question"`
-	CreatedAt time.Time `json:"createdAt"`
-}
-
-var mockQuestions = []question{
-	{Id: 1, QText: "今の気分は？"},
-	{Id: 2, QText: "今日の夕食は？"},
-	{Id: 3, QText: "〇〇でいい感じ！"},
-}
-
-var mockDiaries = []daiary{
-	{Id: 1, Note: "最高!", UserId: 1, Question: mockQuestions[0], CreatedAt: time.Now()},
-	{Id: 2, Note: "ぼちぼち", UserId: 1, Question: mockQuestions[0], CreatedAt: time.Now()},
-}
 
 func main() {
 	enverr := godotenv.Load()
